9f8266df42cf452fb2a32eb148daa2cd0844b4a6
kvsaijagadeesh2007 <saijagadeeshkolipakula@gmail.com>
2026-03-17 11:21:13 +0530

    new UC6

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 enum RoomType { SINGLE, DOUBLE, DELUXE }
 
+class Room {
+    int id;
+    RoomType type;
+    boolean booked = false;
+
+    Room(int id, RoomType type) {
+        this.id = id;
+        this.type = type;
+    }
+}
+
 class Request {
     String name;
     RoomType type;
@@ -13,29 +24,36 @@ class Request {
 }
 
 class Hotel {
-    Map<RoomType, Integer> inv = new HashMap<>();
+    List<Room> rooms = new ArrayList<>();
     Queue<Request> q = new LinkedList<>();
 
     Hotel() {
-        inv.put(RoomType.SINGLE, 2);
-        inv.put(RoomType.DOUBLE, 2);
-        inv.put(RoomType.DELUXE, 1);
+        for (int i = 1; i <= 2; i++) rooms.add(new Room(i, RoomType.SINGLE));
+        for (int i = 3; i <= 4; i++) rooms.add(new Room(i, RoomType.DOUBLE));
+        rooms.add(new Room(5, RoomType.DELUXE));
     }
 
-    void add(String n, RoomType t) {
-        q.add(new Request(n, t));
-        System.out.println("Added: " + n);
+    void add(String name, RoomType type) {
+        q.add(new Request(name, type));
+        System.out.println("Request added: " + name);
     }
 
     void process() {
         while (!q.isEmpty()) {
             Request r = q.poll();
-            if (inv.get(r.type) > 0) {
-                inv.put(r.type, inv.get(r.type) - 1);
-                System.out.println("Booked: " + r.name);
-            } else {
-                System.out.println("Failed: " + r.name);
+            boolean allocated = false;
+
+            for (Room room : rooms) {
+                if (!room.booked && room.type == r.type) {
+                    room.booked = true;
+                    System.out.println("Confirmed: " + r.name + " -> Room " + room.id);
+                    allocated = true;
+                    break;
+                }
             }
+
+            if (!allocated)
+                System.out.println("Failed: " + r.name + " (No rooms)");
         }
     }
 }
@@ -46,7 +64,7 @@ public class BookMyStayApp {
         Hotel h = new Hotel();
 
         while (true) {
-            System.out.println("\n1.Add 2.Process 3.Exit");
+            System.out.println("\n1.Add Request 2.Process 3.Exit");
             int ch = sc.nextInt();
 
             if (ch == 1) {
