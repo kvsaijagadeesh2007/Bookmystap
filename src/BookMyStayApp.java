5c1e4d5c7bf2d6c35ef58c72a9c18fb0d571b81a
kvsaijagadeesh2007 <saijagadeeshkolipakula@gmail.com>
2026-03-17 11:24:44 +0530

    new UC7

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 enum RoomType { SINGLE, DOUBLE, DELUXE }
+enum Service { WIFI, FOOD, LAUNDRY }
 
 class Room {
     int id;
@@ -16,10 +17,12 @@ class Room {
 class Request {
     String name;
     RoomType type;
+    List<Service> services;
 
-    Request(String n, RoomType t) {
+    Request(String n, RoomType t, List<Service> s) {
         name = n;
         type = t;
+        services = s;
     }
 }
 
@@ -33,27 +36,29 @@ class Hotel {
         rooms.add(new Room(5, RoomType.DELUXE));
     }
 
-    void add(String name, RoomType type) {
-        q.add(new Request(name, type));
+    void add(String name, RoomType type, List<Service> services) {
+        q.add(new Request(name, type, services));
         System.out.println("Request added: " + name);
     }
 
     void process() {
         while (!q.isEmpty()) {
             Request r = q.poll();
-            boolean allocated = false;
+            boolean done = false;
 
             for (Room room : rooms) {
                 if (!room.booked && room.type == r.type) {
                     room.booked = true;
-                    System.out.println("Confirmed: " + r.name + " -> Room " + room.id);
-                    allocated = true;
+                    System.out.println("Confirmed: " + r.name +
+                            " -> Room " + room.id +
+                            " Services: " + r.services);
+                    done = true;
                     break;
                 }
             }
 
-            if (!allocated)
-                System.out.println("Failed: " + r.name + " (No rooms)");
+            if (!done)
+                System.out.println("Failed: " + r.name);
         }
     }
 }
@@ -64,14 +69,25 @@ public class BookMyStayApp {
         Hotel h = new Hotel();
 
         while (true) {
-            System.out.println("\n1.Add Request 2.Process 3.Exit");
+            System.out.println("\n1.Add 2.Process 3.Exit");
             int ch = sc.nextInt();
 
             if (ch == 1) {
                 String name = sc.next();
                 int t = sc.nextInt();
+
+                List<Service> services = new ArrayList<>();
+                System.out.println("Add services (1.WIFI 2.FOOD 3.LAUNDRY, 0 to stop)");
+
+                while (true) {
+                    int s = sc.nextInt();
+                    if (s == 0) break;
+                    if (s >= 1 && s <= 3)
+                        services.add(Service.values()[s - 1]);
+                }
+
                 if (t >= 1 && t <= 3)
-                    h.add(name, RoomType.values()[t - 1]);
+                    h.add(name, RoomType.values()[t - 1], services);
             }
             else if (ch == 2) h.process();
             else break;
