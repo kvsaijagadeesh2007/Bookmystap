8de484cd12cea10d17e59c8d96ecb29a065f8e35
kvsaijagadeesh2007 <saijagadeeshkolipakula@gmail.com>
2026-03-17 11:15:21 +0530

    new UC5

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -2,40 +2,40 @@ import java.util.*;
 
 enum RoomType { SINGLE, DOUBLE, DELUXE }
 
+class Request {
+    String name;
+    RoomType type;
+
+    Request(String n, RoomType t) {
+        name = n;
+        type = t;
+    }
+}
+
 class Hotel {
-    private Map<RoomType, Integer> inventory = new HashMap<>();
+    Map<RoomType, Integer> inv = new HashMap<>();
+    Queue<Request> q = new LinkedList<>();
 
-    public Hotel() {
-        inventory.put(RoomType.SINGLE, 3);
-        inventory.put(RoomType.DOUBLE, 3);
-        inventory.put(RoomType.DELUXE, 2);
+    Hotel() {
+        inv.put(RoomType.SINGLE, 2);
+        inv.put(RoomType.DOUBLE, 2);
+        inv.put(RoomType.DELUXE, 1);
     }
 
-    // Show all availability
-    public void showAll() {
-        System.out.println("\nRoom Availability:");
-        for (RoomType type : inventory.keySet()) {
-            System.out.println(type + " : " + inventory.get(type));
-        }
+    void add(String n, RoomType t) {
+        q.add(new Request(n, t));
+        System.out.println("Added: " + n);
     }
 
-    // Search specific room type
-    public void searchRoom(RoomType type) {
-        int count = inventory.get(type);
-        if (count > 0) {
-            System.out.println(type + " rooms available: " + count);
-        } else {
-            System.out.println(type + " rooms NOT available!");
-        }
-    }
-
-    // Book room after checking availability
-    public void bookRoom(RoomType type) {
-        if (inventory.get(type) > 0) {
-            inventory.put(type, inventory.get(type) - 1);
-            System.out.println(type + " room booked!");
-        } else {
-            System.out.println("Booking failed! No " + type + " rooms.");
+    void process() {
+        while (!q.isEmpty()) {
+            Request r = q.poll();
+            if (inv.get(r.type) > 0) {
+                inv.put(r.type, inv.get(r.type) - 1);
+                System.out.println("Booked: " + r.name);
+            } else {
+                System.out.println("Failed: " + r.name);
+            }
         }
     }
 }
@@ -43,38 +43,20 @@ class Hotel {
 public class BookMyStayApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Hotel hotel = new Hotel();
+        Hotel h = new Hotel();
 
         while (true) {
-            System.out.println("\n1.View All 2.Search Room 3.Book Room 4.Exit");
-            int choice = sc.nextInt();
+            System.out.println("\n1.Add 2.Process 3.Exit");
+            int ch = sc.nextInt();
 
-            if (choice == 1) {
-                hotel.showAll();
-            }
-            else if (choice == 2 || choice == 3) {
-                System.out.println("1.SINGLE 2.DOUBLE 3.DELUXE");
+            if (ch == 1) {
+                String name = sc.next();
                 int t = sc.nextInt();
-
-                if (t < 1 || t > 3) {
-                    System.out.println("Invalid!");
-                    continue;
-                }
-
-                RoomType type = RoomType.values()[t - 1];
-
-                if (choice == 2)
-                    hotel.searchRoom(type);
-                else
-                    hotel.bookRoom(type);
+                if (t >= 1 && t <= 3)
+                    h.add(name, RoomType.values()[t - 1]);
             }
-            else if (choice == 4) {
-                System.out.println("Thank you!");
-                break;
-            }
-            else {
-                System.out.println("Invalid choice!");
-            }
+            else if (ch == 2) h.process();
+            else break;
         }
     }
 }
