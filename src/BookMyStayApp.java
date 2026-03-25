391dc5e5a4d76183122a2bffea7c04dd008ac305
kvsaijagdeesh2007 <saijagadeeshkolipakula@gmail.com>
2026-03-25 10:23:04 +0530

    UC11

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -4,31 +4,33 @@ public class BookMyStayApp {
 
     static Map<String, Set<Integer>> rooms = new HashMap<>();
     static Map<String, Set<Integer>> booked = new HashMap<>();
-    static Map<String, String> bookings = new HashMap<>();
 
-    static void addHotel(String h, Integer... r) {
-        rooms.put(h, new HashSet<>(Arrays.asList(r)));
-        booked.put(h, new HashSet<>());
+    static synchronized boolean book(String hotelId, int room) {
+        if (!rooms.containsKey(hotelId) || booked.get(hotelId).contains(room)) return false;
+        booked.get(hotelId).add(room);
+        return true;
     }
 
-    static void book(String id, String h, int r) {
-        if (!rooms.containsKey(h) || booked.get(h).contains(r))
-            throw new RuntimeException("Room unavailable");
-        booked.get(h).add(r);
-        bookings.put(id, h + "-" + r);
+    static void addHotel(String hotelId, Integer... roomNumbers) {
+        rooms.put(hotelId, new HashSet<>(Arrays.asList(roomNumbers)));
+        booked.put(hotelId, new HashSet<>());
     }
 
-    static void cancel(String id) {
-        String[] d = bookings.get(id).split("-");
-        booked.get(d[0]).remove(Integer.parseInt(d[1]));
-        bookings.remove(id);
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         addHotel("H1", 101, 102, 103);
-        book("B1", "H1", 101);
-        book("B2", "H1", 102);
-        cancel("B1");
-        System.out.println(bookings);
+
+        Runnable user1 = () -> System.out.println("User1 booking 101: " + book("H1", 101));
+        Runnable user2 = () -> System.out.println("User2 booking 101: " + book("H1", 101));
+
+        Thread t1 = new Thread(user1);
+        Thread t2 = new Thread(user2);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Booked rooms in H1: " + booked.get("H1"));
     }
 }
